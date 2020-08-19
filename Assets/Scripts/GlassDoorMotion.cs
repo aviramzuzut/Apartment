da010a776c537bcde011e43b89279229eeb9af45
aviramzuzut <42522446+aviramzuzut@users.noreply.github.com>
2020-08-20 02:01:42 +0300

    Add files via upload
    adding door scripts

---
+++ Assets/Scripts/GlassDoorMotion.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassDoorMotion : MonoBehaviour
+{
+    bool isOpen;
+    Animator animator;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpen = false;
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag =="RedKnight" /*|| other.gameObject.tag == "MainCamera"*/)
+        {
+            isOpen = true;
+            animator.SetTrigger("GlassOpen");
+            audioSource.Play();
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(isOpen)
+        {
+            isOpen = false;
+            animator.SetTrigger("GlassClose");
+            audioSource.Play();
+        }
+    }
+}
